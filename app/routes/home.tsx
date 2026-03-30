e3178a9c8b44635e5d26c250feb52ff55d79fc77
Mahmoud Lela <mahmoudmsha4ben@gmail.com>
2026-03-30 11:37:38 +0200

    feat: add simple navbar

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,4 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
@@ -9,11 +10,12 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-    <div className="main-section">
+    <Navbar/>
+    <section className="main-section">
       <div className="page-heading">
         <h1>Track Your Applications & Resume Ratings</h1>
         <h2>Review your submissions and check AI-powered feedback</h2>
       </div>
-    </div>
+    </section>
   </main>;
 }
